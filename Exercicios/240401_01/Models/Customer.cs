ba6efde703561425b404a3e30c23f0c944ca4223
laiscastilho <laynefirmino123@gmail.com>
2024-04-02 13:32:32 -0300

    Aula010424

---
+++ Exercicios/240401_01/Models/Customer.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace _240401_01.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+
+        public Customer()
+        {
+            
+        }
+
+        public Customer(int id)
+        {
+            CustomerId = id;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if(string.IsNullOrWhiteSpace(Name)) 
+                isValid = false;
+
+            return isValid;
+        }
+
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public void Save(Customer customer)
+        {
+
+        }
+    }
+}
---
+++ Exercicios/240401_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using _240401_01.Models;
+
+Customer c1 = new Customer();
+c1.Name = "Jaguara";
+c1.EmailAddress = "jaguara@pixote.net";
+
+// Construtor por Instanciação
+Customer c2 = new Customer(2);
+c1.Name = "Boca-Mole";
+c1.EmailAddress = "bocamole@pixote.net";
+
+// Construtor por Atribuição 
+Customer c3 = new Customer{
+    CustomerId = 3,
+    Name = "Nerso",
+    EmailAddress = "nerso@pixote.net"
+};
